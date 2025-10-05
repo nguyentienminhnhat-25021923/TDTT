ce7c5faeeac36a2974cba69809a3ac28e1c522de
Nguyen Tien Minh Nhat <nhat2k7ok@gmail.com>
2025-10-05 22:16:38 +0700

    bài 8

---
+++ Buổi 3/bài 7.py
@@ -0,0 +1,6 @@
+a=(int(input("nhập số nguyên a: ")))
+b=(int(input("nhập số nguyên b: ")))
+if a+b<0 and a*b>0:
+    print("True")
+else:
+    print("False")
---
+++ Buổi 3/bài 8.py
@@ -0,0 +1,6 @@
+a=input("nhập chuỗi a(kí tự thường): ")
+b=input("nhập chuỗi b(kí tự thường): ")
+if len(a) > len (b):
+    print("True")
+else:
+    print("False")
