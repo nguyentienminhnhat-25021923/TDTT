e0be5019ccc9efda008ed4f54a8d0ef64118bb13
Nguyen Tien Minh Nhat <nhat2k7ok@gmail.com>
2026-01-29 00:52:13 +0700

    bt

---
+++ Buổi 8/W8A11.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def get_normal(p1, p2, p3):
+    v1 = np.array(p2) - np.array(p1)
+    v2 = np.array(p3) - np.array(p1)
+    return np.cross(v1, v2)
+
--- Buổi 8/W8A2.py
+++
@@ -1,9 +0,0 @@
-x,y=map(int,input().split())
-if x==y==0:
-    print("M là gốc toạ độ")
-elif x==0 and y!=0:
-    print(f"M nằm trên trục hoành và cách O {y} m")
-elif x!=0 and y==0:
-    print(f"M nằm trên trục tung và cách O {x} m")
-else:
-    print(f"M cách O {((x**2+y**2)**0.5):.2f} m")
