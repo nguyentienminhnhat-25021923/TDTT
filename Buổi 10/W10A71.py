68c04ed6a77dd5a52d9fb3256f036d52287d4b5d
Nguyen Tien Minh Nhat <nhat2k7ok@gmail.com>
2026-01-29 16:22:47 +0700

    bt

--- Buổi 10/W10A71.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-k = 2* n + 1
-m = n + 1
-for i in range(1, k + 1):
-    c = abs(m - i)
-    s = k - 2 * c
-    print('. ' * c  + '* '* s + '. ' * c)
-            
--- Buổi 9/W9A9.py
+++
@@ -1,5 +0,0 @@
-a,b,c,d,e=map(int,input().split())
-L = [a, b, c, d, e]
-L.remove(max(L))
-L.remove(max(L))
-print(max(L))
